b3d6c35ccac11503521be3453c68e185d9b92ef1
jmathison21 <jennamath21@gmail.com>
2024-03-03 12:35:03 -0500

    layout and resources

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DiceMath - Project 1 for CIS436
-Arithmetic Game using Dice - Views Activity
-Uploaded to git for easy code sharing between devices
+Arithmetic Game using Dice - Views Activity   
+Uploaded to git for easy code sharing between devices   
--- app/src/main/java/com/example/dicemath_cis436/MainActivity.kt
+++ app/src/main/java/com/example/dicemath_cis436/MainActivity.kt
@@ -3,6 +3,18 @@ package com.example.dicemath_cis436
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//constants
+const val addSubRange = 100
+const val mulRange = 21
+const val winPoints = 20
+const val jackpotStart = 5
+
+//global variables
+var currentPlayer = 1
+var player1Points = 0
+var player2Points = 0
+var jackpot = jackpotStart
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
